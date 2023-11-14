975b096892c20a5fb6f7ce831b9edafe45b7c382
James Olsen <olsenjames1116@gmail.com>
2023-11-14 17:35:40 -0500

    Move bio to a component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,10 @@
-import { profileImage, skillsImages } from "../../assets/images";
+import { skillsImages } from "../../assets/images";
+import Bio from "../Bio/Bio";
 
 const About = () => {
     return (
       <section id="about">
-        <div>
-          <h2>About</h2>
-          <img style={{ height: '200px', width: 'auto' }} src={profileImage} />
-          <p>
-            Hello, I&#39;m James. I am a self-taught, motivated developer with a
-            year of experience building web applications. With a background in
-            hospitality and leading Marines, I welcome challenges and enjoy
-            problem-solving, especially as part of a team. Facing each day with a
-            growth mindset, I am willing and able to learn. I hope to one day
-            apply my skills and experience, along with my passion for web
-            development, to the right company.
-          </p>
-        </div>
+        <Bio />
         <ul>
           {skillsImages.map((skill) => {
             return (
