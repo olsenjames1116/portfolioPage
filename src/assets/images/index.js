732d9849f9dba212e850d1f5dff9cd5afc106237
James Olsen <olsenjames1116@gmail.com>
2023-11-14 17:22:10 -0500

    Download and structure skill icons
    Skills icons were found and downloaded. The images were then placed
    inside an object to be accessed and displayed.

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,28 @@
 import heroImage from "./hero.jpg";
 import profileImage from "./profile.jpg";
+import cssImage from "./css.svg";
+import eslintImage from "./eslint.svg";
+import firebaseImage from "./firebase.svg";
+import gitImage from "./git.svg";
+import githubImage from "./github.svg";
+import htmlImage from "./html.svg";
+import javascriptImage from "./javascript.svg";
+import jestImage from "./jest.png";
+import linuxImage from "./linux.png";
+import nodejsImage from "./nodejs.svg";
+import npmImage from "./npm.svg";
+import prettierImage from "./prettier.svg";
+import reactImage from "./react.svg";
+import reduxImage from "./redux.svg";
+import styledComponentsImage from "./styledComponents.png";
+import viteImage from "./vite.svg";
+import webpackImage from "./webpack.svg";
 
-export { heroImage, profileImage };
+const skillsImages = [{name: "JavaScript", src: javascriptImage}, {name: "HTML", src: htmlImage}, 
+    {name: "CSS", src: cssImage}, {name: "Git", src: gitImage},  {name: "React", src: reactImage}, 
+    {name: "Vite", src: viteImage}, {name: "Redux", src: reduxImage}, {name: "styled-components", src: styledComponentsImage}, 
+    {name: "Jest", src: jestImage}, {name: "NodeJS", src: nodejsImage}, {name: "Webpack", src: webpackImage}, 
+    {name: "npm", src: npmImage}, {name: "ESLint", src: eslintImage}, {name: "Prettier", src: prettierImage}, 
+    {name: "Firebase", src: firebaseImage}, {name: "GitHub", src: githubImage}, {name: "Linux", src: linuxImage}]
+
+export { heroImage, profileImage, skillsImages };
