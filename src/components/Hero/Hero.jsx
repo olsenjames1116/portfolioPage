a880928fd1dafea657dc210bc2e78ec481bf430b
James Olsen <olsenjames1116@gmail.com>
2023-11-14 15:58:28 -0500

    Change element in Hero from div to section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,11 @@ import {heroImage} from "../../assets/images";
 
 const Hero = () => {
     return (    
-        <div>
+        <section>
             <img style={{width: "100vw", height: "auto"}} src={heroImage} alt=""></img>
             <h1>James Olsen</h1>
             <h1>Full Stack Web Developer</h1>
-        </div>
+        </section>
     );
 }
 
