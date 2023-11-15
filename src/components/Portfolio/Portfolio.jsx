f954505713bb86e157f11c5e2599a15a69a216f3
James Olsen <olsenjames1116@gmail.com>
2023-11-15 11:05:58 -0500

    Move display for filtered skills into function
    To clean up JSX in the return statement of the component, the algorithm
    for filtering an array of skills to display was moved to its own
    function.

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -20,21 +20,21 @@ const Projects = () => {
     },
     {
       name: 'Task Manager',
-      technologies: [],
+      technologies: ['Webpack', 'npm'],
       summary:
         "Ever have that feeling that you're forgetting something? You're not alone! Meet the Task Manager app. Add tasks with a title, description, due date and priority. Edit existing tasks as their priority or due dates change. Create folders to organize tasks. Finally, use the sidebar to navigate to pre-existing folders that organize tasks by due dates and importance. All of this is stored on your local machine so you can keep track of your responsibilities. You'll never forget the kid at school again!",
       src: taskManagerImage,
     },
     {
       name: 'Jim Class',
-      technologies: [],
+      technologies: ['React', 'Router'],
       summary:
         "A mock gym equipment store. This app allows you to browse the shop, view full-size items on their own pages, add items to the cart and checkout. Time to get to work on those New Year's resolutions you've been putting off.",
       src: jimClassImage,
     },
     {
       name: 'CV Builder',
-      technologies: [],
+      technologies: ['React'],
       summary:
         "Move out of your Mom's basement today! Dynamically create and edit your CV with this convenient app! Create new entries and edit/delete existing ones to build out your resume. All changes can be viewed on the document as they are made to give a preview of the final product.",
       src: cvBuilderImage,
@@ -50,10 +50,15 @@ const Projects = () => {
     return filteredSkills;
   };
 
-  // const displayFilteredSkills = project => {
-  //   const filteredSkills = filterSkills(project);
-  //   console.log(filteredSkills.forEach(skill => skill.name));
-  // };
+  const displayFilteredSkills = project => {
+    const filteredSkills = filterSkills(project);
+
+    return filteredSkills.map((skill, index) => (
+      <li key={index}>
+        <img style={{ height: '50px', width: 'auto' }} src={skill.src} />
+      </li>
+    ));
+  };
 
   return (
     <section id="portfolio">
@@ -63,16 +68,7 @@ const Projects = () => {
           return (
             <li key={index}>
               <h3>{project.name}</h3>
-              <ul>
-                {filterSkills(project).map((skill, index) => (
-                  <li key={index}>
-                    <img
-                      style={{ height: '50px', width: 'auto' }}
-                      src={skill.src}
-                    />
-                  </li>
-                ))}
-              </ul>
+              <ul>{displayFilteredSkills(project)}</ul>
               <p>{project.summary}</p>
               <img
                 style={{ width: '50vw', height: 'auto' }}
