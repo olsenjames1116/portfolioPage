412657de7bbee655a42242563aba27cdfa7fb28f
James Olsen <olsenjames1116@gmail.com>
2023-11-15 19:41:45 -0500

    Link to email and GitHub to images in Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,21 @@
 import { emailImage, githubImage, linkedInImage } from '../../assets/images';
 import Image from '../Image/Image';
+import Link from '../Link/Link';
 
 const Contact = () => {
   return (
     <section id="contact">
       <h2>Contact</h2>
       <span>olsenjames1116@gmail.com</span>
-      <Image src={emailImage} />
-      <Image src={githubImage} />
-      <Image src={linkedInImage} />
+      <Link href="mailto:olsenjames1116@gmail.com">
+        <Image src={emailImage} />
+      </Link>
+      <Link href="https://github.com/olsenjames1116" target="_blank">
+        <Image src={githubImage} />
+      </Link>
+      <Link href="" target="_blank">
+        <Image src={linkedInImage} />
+      </Link>
     </section>
   );
 };
--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types';
 
-const Link = ({ href, children }) => {
-    return (
+const Link = ({ href, children, target }) => {
+  return (
+    <>
+      {target ? (
+        <a href={href} target={target}>
+          {children}
+        </a>
+      ) : (
         <a href={href}>{children}</a>
-    )
-}
+      )}
+    </>
+  );
+};
 
 Link.propTypes = {
-    href: PropTypes.string,
-    children: PropTypes.node
-}
+  href: PropTypes.string,
+  children: PropTypes.node,
+  target: PropTypes.string,
+};
 
 export default Link;
