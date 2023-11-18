09c892bb1f8fe93c2d5d5a5240ae413b8c9aba3e
James Olsen <olsenjames1116@gmail.com>
2023-11-18 09:24:24 -0500

    Add an animation to h2 in subheading on scroll

--- src/components/Subheading/Subheading.jsx
+++ src/components/Subheading/Subheading.jsx
@@ -1,12 +1,31 @@
 import Container from '../Container/Container';
 import PropTypes from 'prop-types';
 import styles from './Subheading.module.css';
+import { useEffect, useRef } from 'react';
 
 const Subheading = ({ headingText }) => {
+  const headingRef = useRef(null);
+
+  const addObserver = (element, options = {}) => {
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.active);
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+    observer.observe(element, options);
+  };
+
+  useEffect(() => {
+    addObserver(headingRef.current, { rootMargin: '-200px' });
+  }, []);
+
   return (
     <Container className={styles.subheading}>
       <hr />
-      <h2>{headingText}</h2>
+      <h2 ref={headingRef}>{headingText}</h2>
       <hr />
     </Container>
   );
