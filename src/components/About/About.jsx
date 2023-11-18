fa95b1130baef625e50f7e974d717cdc40064657
James Olsen <olsenjames1116@gmail.com>
2023-11-17 21:15:35 -0500

    Apply some styling to the About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,16 @@
 import Bio from '../Bio/Bio';
 import Skills from '../Skills/Skills';
+import Container from '../Container/Container';
+import './About.css';
 
 const About = () => {
   return (
     <section id="about">
-      <h2>About</h2>
+      <Container className="subheading">
+        <hr />
+        <h2>About</h2>
+        <hr />
+      </Container>
       <Bio />
       <Skills />
     </section>
