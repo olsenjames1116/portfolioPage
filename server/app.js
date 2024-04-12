f2c380abf5b6ace9a5feecaf171b6cb3132217e9
James Olsen <olsenjames1116@gmail.com>
2024-04-12 18:41:35 -0400

    Clean up unused files from server

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,20 +16,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404));
+// Generic error handler.
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send('Something went wrong');
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+// Listen on port when server is started.
+app.listen(port, () => {
+	const url =
+		process.env.NODE_ENV === 'production'
+			? 'https://api.whisk-recipes.com'
+			: `http://localhost:${port}`;
+	console.log(`Server running at ${url}`);
 });
 
 module.exports = app;
