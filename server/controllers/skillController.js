c676c0bab953888ad5883c1d621a5c21743e2e22
James Olsen <olsenjames1116@gmail.com>
2024-04-12 19:22:25 -0400

    Make skeleton for skill controller

--- server/controllers/skillController.js
+++ server/controllers/skillController.js
@@ -1,2 +1,9 @@
 const Skill = require('../models/skill');
 const asyncHandler = require('express-async-handler');
+
+// Get all skills information from the db.
+exports.getSkills = asyncHandler(async (req, res, next) => {
+	const skills = await Skill.find();
+
+	console.log(skills);
+});
--- server/routes/skill.js
+++ server/routes/skill.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const skillController = require('../controllers/skillController');
+
+// GET all the skills from the db.
+router.get('/', skillController.getSkills);
+
+module.exports = router;
